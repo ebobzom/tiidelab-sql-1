50df0eddcd7e326616fc38094306120f3d8e88a8
Elijah Bobzom <ebobzom@gmail.com>
2020-03-31 17:30:46 +0100

    add comment to queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
+-- Question 2
+SELECT * FROM order_items WHERE order_id = 6 && (quantity * unit_price) > 30;
+
+-- Question 3
 SELECT * FROM products WHERE quantity_in_stock = 49 || quantity_in_stock = 38 || quantity_in_stock = 72;
 
-SELECT * FROM order_items WHERE order_id = 6 && (quantity * unit_price) > 30;
